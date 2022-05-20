1b7642eea721fcfcc2bb24e8e96d031d2c695d3a
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-05-20 19:56:13 +0400

    connect xamp server and connect mysql

--- blog.py
+++ blog.py
@@ -1,6 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
 
+#!Create Flask Object
 app = Flask(__name__)
+
+#!Connect Mysql and Xampp Server
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "ybblog"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"#return queryset data as django quersets
+mysql = MySQL(app)
 
 @app.route('/')
 def indexView():
@@ -16,6 +28,10 @@ def aboutView():
         "description":"Welcome my blog site"
     }
     return render_template('blogtemplates/about.html',context=context)
+
+@app.route('/detail/<int:id>')
+def detailView(id):
+    return f"Detail Post Id - {id}"
 
 #!__name__ == '__main__'
 if __name__ == '__main__':
