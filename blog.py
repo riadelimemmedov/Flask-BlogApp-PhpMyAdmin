1fb3760a58999b5b3d9f13bf469a329dd62863fd
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-05-22 21:36:37 +0400

    finish register page

--- blog.py
+++ blog.py
@@ -5,6 +5,7 @@ from passlib.hash import sha256_crypt
 
 #!Create Flask Object
 app = Flask(__name__)
+app.secret_key = 'blogdsa3@*51!3#'
 
 #!Connect Mysql and Xampp Server
 app.config["MYSQL_HOST"] = "localhost"
@@ -14,7 +15,18 @@ app.config["MYSQL_DB"] = "ybblog"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"#return queryset data as django quersets
 mysql = MySQL(app)
 
-#!Views
+#!Register Form
+class RegisterForm(Form):
+    name = StringField(label='Isim Soyisim',validators=[validators.Length(min=4,max=25)])
+    username = StringField(label='Kullanici Adi',validators=[validators.Length(min=5,max=35)])
+    email = StringField(label='Email Adresi',validators=[validators.Email(message='Lutfen Gecerli Bir Email Adresi Girin...')])
+    password = PasswordField(label='Parola:',validators=[
+        validators.DataRequired(message='Lutfen bit parola belirleyin'),
+        validators.EqualTo(fieldname='confirm',message='Parolaniz Uyusmuyor')
+    ])
+    confirm = PasswordField('Parola Tekrar')
+
+#!indexView
 @app.route('/')
 def indexView():
     context = {
@@ -23,6 +35,7 @@ def indexView():
     }
     return render_template('blogtemplates/index.html',context=context)
 
+#!aboutView
 @app.route('/about')
 def aboutView():
     context = {
@@ -30,9 +43,29 @@ def aboutView():
     }
     return render_template('blogtemplates/about.html',context=context)
 
+#!detailView
 @app.route('/detail/<int:id>')
 def detailView(id):
     return f"Detail Post Id - {id}"
+
+@app.route('/register',methods=['GET','POST'])
+def registerView():
+    form = RegisterForm(request.form)#form icindeki deyerleri getirir
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        username = form.username.data 
+        email = form.email.data 
+        password = sha256_crypt.encrypt(form.password.data)
+        
+        cursor = mysql.connection.cursor()
+        query = "INSERT INTO users(name,email,username,password) VALUES(%s,%s,%s,%s)"
+        cursor.execute(query,(name,email,username,password))
+        mysql.connection.commit()
+        cursor.close()
+        flash('Başarıyla Kayıt Oldunuz...','success')
+        return redirect(url_for('indexView'))#yeni indexView adli funksiya isleyende hemin funksiyanin url ni getsin ele bil url_for o isi gorur
+    else:
+        return render_template('blogtemplates/register.html',form=form)
 
 #!__name__ == '__main__'
 if __name__ == '__main__':
